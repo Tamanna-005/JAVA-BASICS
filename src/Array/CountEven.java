30aff547e29477270b9c6e6e9b95de436d450384
Tamanna Rana <tamanna.rana0917@gmail.com>
2026-02-01 21:13:33 +0530

    count even number in array

---
+++ src/Array/CountEven.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+
+public class CountEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your array length: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter you elements: ");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // printing elements
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                count++;
+            }
+        }
+        System.out.print("Total even number in array: "+count);
+    }
+}
