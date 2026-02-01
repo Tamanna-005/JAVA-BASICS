8a60deb188aada79ab75a892c8fd8a4939dce819
Tamanna Rana <tamanna.rana0917@gmail.com>
2026-02-01 20:06:35 +0530

    These are Java Basic questions

---
+++ src/Array/SumOfArray.java
@@ -0,0 +1,30 @@
+package Array;
+import java.util.*;
+
+public class SumOfArray {
+   public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+       System.out.print("Your array length: ");
+       int n=sc.nextInt();
+
+       System.out.println("Enter you elements: ");
+       int arr[] = new int[n];
+       for(int i=0;i<n;i++){
+           arr[i]=sc.nextInt();
+       }
+       // printing elements
+       for(int i=0;i<n;i++){
+           System.out.print(arr[i]+" ");
+       }
+       System.out.println();
+
+        int sum=0;
+
+       for(int i=0;i<n;i++){
+           sum+=arr[i];
+       }
+System.out.println("Sum os array is: "+sum);
+   }
+
+
+}
