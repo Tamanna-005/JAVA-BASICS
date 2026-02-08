621ba717d87abb649bb861bd23fa71efcd5d60cd
Tamanna Rana <tamanna.rana0917@gmail.com>
2026-02-08 13:33:38 +0530

    Added code to print pyramid pattern

--- src/Loop/Pattern.java
+++ src/Loop/Pattern.java
@@ -1,8 +1,9 @@
 package Loop;
 
 import java.util.*;
+// printing right angle triangle
 
-public class Pattern {
+/*public class Pattern {
     static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter length of triangle: ");
@@ -14,6 +15,30 @@ public class Pattern {
                 System.out.print("* ");
             }
             System.out.println();
+        }
+
+    }
+}*/
+
+// printing pyramid
+
+public class Pattern {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter length of pyramid: ");
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){                   //*
+            for(int j=i;j<n-1;j++){            //* *
+                System.out.print(" ");        //* * *
+            }
+
+            for(int k=0;k<i;k++){
+                System.out.print("*"+" ");
+            }
+
+            System.out.println();
+
         }
 
     }
