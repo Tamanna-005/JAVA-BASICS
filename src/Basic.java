b0fb91604b67ff74a3c382d3cb5f1480b3ce0e97
Tamanna Rana <tamanna.rana0917@gmail.com>
2026-02-26 13:29:44 +0530

    Code to create class in OOPS

---
+++ src/Basic.java
@@ -0,0 +1,12 @@
+class Person{
+    int age=20;
+
+}
+
+public class Basic {
+
+   public  void main(String[] args){
+        Person Tanu=new Person();
+        System.out.println(Tanu.age);
+    }
+}
